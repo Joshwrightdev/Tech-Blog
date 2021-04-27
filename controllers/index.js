bce757e5dc359c25e83d1a5d52b042ca6796a5cd
Josh Wright <joshwrightaudio@gmail.com>
2021-04-27 09:26:07 -0400

    server functional

--- controllers/index.js
+++ controllers/index.js
@@ -2,10 +2,7 @@ const router = require("express").Router();
 
 const apiRoutes = require("./api");
 const homeRoutes = require("./homeRoutes");
-const dashRoutes = require("./dashRoutes");
 
-
-router.use("/dashboard", dashRoutes);
 router.use("/", homeRoutes);
 router.use("/api", apiRoutes);
 
