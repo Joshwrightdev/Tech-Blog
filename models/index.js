d0d6adde62d2e7a5a664711e399bfb1718dde3e9
Josh Wright <joshwrightaudio@gmail.com>
2021-04-27 09:48:39 -0400

    server works with seeded data

--- models/index.js
+++ models/index.js
@@ -1,6 +1,5 @@
 const User = require("./User");
 const Post = require("./Post");
-const Comment = require("./Comment");
 
 User.hasMany(Post, {
   foreignKey: "user_id",
@@ -10,24 +9,8 @@ Post.belongsTo(User, {
   foreignKey: "user_id",
 });
 
-Comment.belongsTo(User, {
-  foreignKey: "user_id",
-});
-
-Comment.belongsTo(Post, {
-  foreignKey: "post_id",
-});
-
-User.hasMany(Comment, {
-  foreignKey: "user_id",
-});
-
-Post.hasMany(Comment, {
-  foreignKey: "post_id",
-});
 
 module.exports = {
   User,
   Post,
-  Comment,
 };
