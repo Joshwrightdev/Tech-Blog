7a0f30d5e6bdf06420c9697347566d46e85c45f0
Josh Wright <joshwrightaudio@gmail.com>
2021-05-09 15:50:14 -0400

    cm

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -74,10 +74,10 @@ router.get("/posts", async (req, res) => {
     });
 
     console.log(req.session.logged_in);
-    const Post = PostData.map((Post) => Post.get({ plain: true }));
+    const post = PostData.map((Post) => Post.get({ plain: true }));
 
     res.render("my-posts", {
-      Post,
+      post,
       loggedIn: req.session.logged_in,
     });
   } catch (err) {
